dd77c70a89f00576684b365428479b5cbbd47980
이효규 <lhg961006@gmail.com>
2024-04-16 23:16:21 +0900

    webConfig수정

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,18 +1,22 @@
 package com.example.demo.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final Environment environment;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // 엔드포인트 패턴을 필요에 따라 조정
-                .allowedOriginPatterns("http://13.209.62.108:4000") // 클라이언트 도메인
+                .allowedOriginPatterns(environment.getProperty("allow.url")) // 클라이언트 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
